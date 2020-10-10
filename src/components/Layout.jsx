837d63b71a4cbb2e8a07e2d0d6629cef3c659145
Miyo <miyovihtodenko@gmail.com>
2020-10-10 18:26:14 +0200

    Finish the navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import Navbar from "./Navbar";
 
+import "../styles/components/Layout.scss";
+
 const Layout = (props) => {
   return (
     <div className="Wrapper">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/components/Navbar.scss";
 
 const Navbar = () => {
-  return "Navbar";
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar__container">
+        <Link to="/" className="navbar__logo">
+          <img className="logo" src="" alt="Logo" />
+        </Link>
+        <div
+          className={click ? "mobile-menu open" : "mobile-menu"}
+          onClick={handleClick}
+        >
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <ul className={click ? "navbar__menu active" : "navbar__menu"}>
+          <li className="navbar__item">
+            <Link to="#" className="navbar__link">
+              Language
+            </Link>
+          </li>
+          <li className="navbar__item">
+            <Link
+              to="/about"
+              className="navbar__link"
+              onClick={closeMobileMenu}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
