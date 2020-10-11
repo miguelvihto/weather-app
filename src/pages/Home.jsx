f9dae6c219db0a3ceebcbcc9f077a462b01b5610
Miyo <miyovihtodenko@gmail.com>
2020-10-11 17:20:15 +0200

    Home Page finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+
+import '../styles/pages/Home.scss'
+import HeroImg from '../assets/img/HeroImg2.0.svg'
 
 const Home = () => {
-  return "Home";
+  return (
+    <div className="hero">
+        <div className="hero__banner">
+          <img className="hero__img" src={HeroImg} alt="Main Image"/>
+          <h1>You are going to see the <strong>weather</strong>,</h1> 
+          <br/>
+          <h1>as you never see...</h1>
+        </div>
+        <div className="hero__cta">
+          <Link to="#" className="cta__link">
+            Go! See the weather
+          </Link>
+        </div>
+    </div>
+  );
 };
 
 export default Home;
