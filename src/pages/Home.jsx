32b316039dfad6328f8b6e9d14194cf9b232271a
Miyo <miyovihtodenko@gmail.com>
2020-10-12 10:57:54 +0200

    Now the home is responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,11 @@ const Home = () => {
     <div className="hero">
         <div className="hero__banner">
           <img className="hero__img" src={HeroImg} alt="Main Image"/>
-          <h1>You are going to see the <strong>weather</strong>,</h1> 
-          <br/>
-          <h1>as you never see...</h1>
+          <div className="hero__text">
+            <h1>You are going to see the <strong>weather</strong>,</h1> 
+            <br/>
+            <h1>as you never see...</h1>
+          </div>
         </div>
         <div className="hero__cta">
           <Link to="#" className="cta__link">
